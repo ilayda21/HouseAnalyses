d31716ad948c5fc6ca69e0095a948d5fb9a724e9
ilayda21 <cavusogluilayda@gmail.com>
2020-12-22 12:36:36 +0300

    Some processing are done

--- HousePricing.R
+++ HousePricing.R
@@ -1,12 +1,13 @@
 library(CORElearn)
 library(tibble)
+library(tidyr)
 
 load("house.data")
 
 data <- antalya_train
 
 dataFrame <- data.frame(data)
-dim(data)
+dataDimension <- dim(data)
 
 dataFrame[dataFrame==""]<-NA
 
@@ -25,9 +26,74 @@ print(naTable)
 
 # by using attr eval, we are going to calculate the correlation between target value and all others
 # because we want to eliminate some unrelated columns (attrEval -> CORElearn)
-attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
+# attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
 
 dataFrame$MustakilMi <- factor(dataFrame$MustakilMi, labels = c("no", "yes"))
-print(dataFrame$MustakilMi)
+
+# odasayisi+salonsayisi (factor)
+dataFrame$OdaBilgisi <- NA
+
+for (i in 1:dataDimension[1]) {
+  dataFrame$OdaBilgisi[i] <- paste(dataFrame$OdaSayisi[i], "+", dataFrame$SalonSayisi[i])
+}
+
+# attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
+
+# manzara => make it factor (Doga, Sehir, Deniz, Bogaz)
+dataFrame$Manzara <- NA
+for (i in 1:dataDimension[1]) {
+  viewValue <- ""
+  if (dataFrame$ManzaraGol[i] == 1) {
+    viewValue = paste0(viewValue, "Gol")
+  } 
+  
+  if(dataFrame$ManzaraDeniz[i] == 1) {
+    viewValue = paste0(viewValue, "Deniz")
+  }
+  
+  if (dataFrame$ManzaraBogaz[i] == 1) {
+    viewValue = paste0(viewValue, "Bogaz")
+  }
+  
+  if (dataFrame$ManzaraSehir[i] == 1) {
+    viewValue = paste0(viewValue, "Sehir")
+  }  
+  
+  if (dataFrame$ManzaraDoga[i] == 1) {
+    viewValue = paste0(viewValue, "Doga")
+  }
+  
+  if (viewValue == "") {
+    viewValue <- "ManzaraYok"
+  } 
+  
+  dataFrame$Manzara[i] <- viewValue
+}
+
+# dataFrame
+# attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
+
+# cephe => Bati Dogu Guney Kuzey
+dataFrame$Cephe <- NA
+for (i in 1:dataDimension[1]) {
+  frontValue <- ""
+  if (dataFrame$Kuzey[i] == 1) {
+    frontValue = paste0(viewValue, "Kuzey")
+  } 
+  
+  if(dataFrame$Guney[i] == 1) {
+    frontValue = paste0(viewValue, "Guney")
+  }
+  
+  if (dataFrame$Dogu[i] == 1) {
+    frontValue = paste0(viewValue, "Dogu")
+  }
+  
+  if (dataFrame$Bati[i] == 1) {
+    frontValue = paste0(viewValue, "Bati")
+  }  
+  
+  dataFrame$Cephe[i] <- frontValue
+}
 
 tibbledData = as_tibble(dataFrame)
