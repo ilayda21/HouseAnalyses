125b56a18dfeccaa2e1d382b7ed8144fa4085997
ilayda21 <cavusogluilayda@gmail.com>
2020-12-22 08:45:33 +0300

    read and process data

--- HousePricing.R
+++ HousePricing.R
@@ -1,3 +1,5 @@
+library(CORElearn)
+library(tibble)
 
 load("house.data")
 
@@ -11,11 +13,21 @@ dataFrame[dataFrame==""]<-NA
 # Id is removed because for each row is unique
 dataFrame$Id <- NULL
 
-dim(dataFrame)
-names(dataFrame)
-
+# Type of each column
+print("Column Types: ")
 columnTypes <- sapply(dataFrame, class)
+print(columnTypes)
 
+# number of NA for each column
+print("Number of Nas for each column")
+naTable <- colSums(is.na(dataFrame))
+print(naTable)
 
+# by using attr eval, we are going to calculate the correlation between target value and all others
+# because we want to eliminate some unrelated columns (attrEval -> CORElearn)
+attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
+
+dataFrame$MustakilMi <- factor(dataFrame$MustakilMi, labels = c("no", "yes"))
+print(dataFrame$MustakilMi)
 
 tibbledData = as_tibble(dataFrame)
