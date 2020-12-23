ea0c076fe206e51d21e4665959c778807c2c3370
ilayda21 <cavusogluilayda@gmail.com>
2020-12-23 23:06:47 +0300

    Some processing and outlier detection are done

--- HousePricing.R
+++ HousePricing.R
@@ -1,6 +1,12 @@
 library(CORElearn)
 library(tibble)
 library(tidyr)
+library(knitr)
+library(dplyr)
+library(arules)
+library(arulesViz)
+library(cluster)
+library(fpc)
 
 load("house.data")
 
@@ -64,7 +70,7 @@ for (i in 1:dataDimension[1]) {
   }
   
   if (viewValue == "") {
-    viewValue <- "ManzaraYok"
+    viewValue <- "ManazaraYok"
   } 
   
   dataFrame$Manzara[i] <- viewValue
@@ -73,27 +79,159 @@ for (i in 1:dataDimension[1]) {
 # dataFrame
 # attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
 
+
+dataFrame = subset(dataFrame, select = -c(OdaSayisi,SalonSayisi, ToplamOdaSayisi,
+                                          ManzaraSehir, ManzaraDoga, ManzaraGol,
+                                          ManzaraDeniz, ManzaraBogaz))
+
+# --------------------------------------AGE------------------------------------
+
+for (i in 1:dataDimension[1]) {
+  
+  if(is.na(dataFrame$GercekYas[i])) {
+      firstFilter <- dataFrame[dataFrame$Ilce == dataFrame$Ilce[i] &
+                        dataFrame$Mahalle == dataFrame$Mahalle[i] &
+                        dataFrame$OdaBilgisi == dataFrame$OdaBilgisi[i],]
+      
+      if(dim(firstFilter)[1] == 0) {
+        print("CANNOT PASS FIRST FILTER")
+      } else {
+        x <- firstFilter[firstFilter$BanyoSayisi == dataFrame$BanyoSayisi[i]  & 
+                           firstFilter$BulunduguKat > dataFrame$BulunduguKat[i] - 3 
+                         & firstFilter$BulunduguKat < dataFrame$BulunduguKat[i] + 3
+                         & firstFilter$Fiyat < dataFrame$Fiyat[i] + 20000
+                         & firstFilter$Fiyat > dataFrame$Fiyat[i] - 20000,]
+        if (dim(firstFilter)[1] == 0) {
+          print("CANNOT PASS SECOND FILTER")
+        } else {
+          y <- x[x$OrijinalAlan  < dataFrame$OrijinalAlan [i] + 30
+                   & x$OrijinalAlan  > dataFrame$OrijinalAlan [i] - 30,]
+          medianResult <- (median(y$GercekYas, na.rm=TRUE))
+          if (!is.na(medianResult)) {
+            dataFrame$GercekYas[i] <- medianResult
+          }
+        }
+      }
+
+  }
+}
+
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
 # cephe => Bati Dogu Guney Kuzey
 dataFrame$Cephe <- NA
+naTable <- colSums(is.na(dataFrame))
+print(naTable)
+
 for (i in 1:dataDimension[1]) {
   frontValue <- ""
-  if (dataFrame$Kuzey[i] == 1) {
-    frontValue = paste0(viewValue, "Kuzey")
+  if (dataFrame$CepheKuzey[i] == 1) {
+    frontValue = paste0(frontValue, "Kuzey")
   } 
   
-  if(dataFrame$Guney[i] == 1) {
-    frontValue = paste0(viewValue, "Guney")
+  if(dataFrame$CepheGuney[i] == 1) {
+    frontValue = paste0(frontValue, "Guney")
   }
   
-  if (dataFrame$Dogu[i] == 1) {
-    frontValue = paste0(viewValue, "Dogu")
+  if (dataFrame$CepheDogu[i] == 1) {
+    frontValue = paste0(frontValue, "Dogu")
   }
   
-  if (dataFrame$Bati[i] == 1) {
-    frontValue = paste0(viewValue, "Bati")
+  if (dataFrame$CepheBati[i] == 1) {
+    frontValue = paste0(frontValue, "Bati")
   }  
+  
+  if(frontValue == "") {
+    if(dataFrame$MustakilMi[i]=="yes") {
+      frontValue = "KuzeyGuneyDoguBati"
+    } else {
+      
+      firstFilter <- dataFrame[dataFrame$Ilce == dataFrame$Ilce[i] &
+                        dataFrame$Mahalle == dataFrame$Mahalle[i] &
+                        dataFrame$MustakilMi[i]=="no" &
+                        dataFrame$OdaBilgisi == dataFrame$OdaBilgisi[i] &
+                        dataFrame$BanyoSayisi == dataFrame$BanyoSayisi[i]  & 
+                        dataFrame$BulunduguKat > dataFrame$BulunduguKat[i] - 3 
+                      & dataFrame$BulunduguKat < dataFrame$BulunduguKat[i] + 3
+                      & dataFrame$Fiyat < dataFrame$Fiyat[i] + 20000
+                      & dataFrame$Fiyat > dataFrame$Fiyat[i] - 20000,]
+      if(dim(firstFilter)[1] == 0) {
+        print("CANNOT FOUND")
+        frontValue <- NA
+      } else {
+        
+        medianBatiResult <- median(firstFilter$CepheBati, na.rm=TRUE)
+        if (!is.na(medianBatiResult) && medianBatiResult == 1) {
+          frontValue = paste0(frontValue, "Bati")
+        } 
+        
+        medianDoguResult <- median(firstFilter$CepheDogu, na.rm=TRUE)
+        if (!is.na(medianDoguResult) && medianDoguResult == 1) {
+          frontValue = paste0(frontValue, "Dogu")
+        } 
+        
+        medianGuneyResult <- median(firstFilter$CepheGuney, na.rm=TRUE)
+        if (!is.na(medianGuneyResult) && medianGuneyResult == 1) {
+          frontValue = paste0(frontValue, "Guney")
+        } 
+        
+        medianKuzeyResult <- median(firstFilter$CepheKuzey, na.rm=TRUE)
+        if (!is.na(medianKuzeyResult) && medianKuzeyResult == 1) {
+          frontValue = paste0(frontValue, "Kuzey")
+        } 
+        
+        if(frontValue == "") {
+           frontValue <- NA
+        } 
+      }
+
+    }
+  }
   
   dataFrame$Cephe[i] <- frontValue
 }
 
+naTable <- colSums(is.na(dataFrame))
+print(naTable)
+
+# ---------------------------------------------------------------------------
+
+dataFrame = subset(dataFrame, select = -c(CepheBati,CepheDogu, CepheGuney,
+                                          CepheKuzey))
+
+print(dim(dataFrame))
+dataFrame <- dataFrame[complete.cases(dataFrame), ]
+print(dim(dataFrame))
+
+# attrEval(FiyatTL ~ . , dataFrame, estimator = "Gini")
+
+detect_outliers <- function(x, upperLimit, lowerLimit)
+{
+  Q1 <- quantile(x, lowerLimit)
+  Q3 <- quantile(x, upperLimit)
+  IQR <- Q3 - Q1
+  Vl <- Q1 - 1.5 * IQR
+  Vr <- Q3 + 1.5 * IQR
+  return (which(x < Vl | x > Vr))
+}
+
+# find Outlier with IQR
+
+# OrijinalAlan 
+print(detect_outliers(dataFrame$OrijinalAlan, 0.985, 0.015))
+
+# AlanMetrekare 
+print(detect_outliers(dataFrame$AlanMetrekare, 0.985, 0.015))
+
+# FiyatTL  
+print(detect_outliers(dataFrame$FiyatTL, 0.985, 0.015))
+
+
+
+# find Outlier with DBSCAN
+
+
+
 tibbledData = as_tibble(dataFrame)
+
